a31f0ce9e817bccb547eacf6ab0db9ebf383639f
Sabriel Gee <sabrielgee@sabriels-imac.lan>
2022-01-30 14:49:17 -0700

    password encryption

--- models/User.js
+++ models/User.js
@@ -1,7 +1,12 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
-class User extends Model {}
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
 User.init(
     {
@@ -32,8 +37,19 @@ User.init(
         }
 
     },
-
     {
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
+    
+
         sequelize,
         timestamps: false,
         freezeTableName: true,
@@ -41,5 +57,6 @@ User.init(
         modelName: 'user'
     }
 );
+
 
 module.exports = User;
