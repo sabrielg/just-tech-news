1605be70e2a0def95ae6874c7f1c66cf07af998c
Sabriel Gee <sabrielgee@sabriels-imac.lan>
2022-01-30 18:50:54 -0700

    post routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -57,6 +57,21 @@ router.post('/login', (req, res) => {
     });
   });
 
+// POST /api/users
+router.post('/', (req, res) => {
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+    User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    })
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
     User.update(req.body, {
