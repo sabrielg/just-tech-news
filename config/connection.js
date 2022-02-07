9b4db925b895b6a5de635f9bc74b213de5f897ec
Sabriel Gee <sabrielgee@sabriels-imac.lan>
2022-02-07 16:29:12 -0700

    sequelize fix

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize');
 let sequelize;
+require('dotenv').config();
 
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
@@ -12,13 +13,10 @@ if (process.env.JAWSDB_URL) {
 }
 
 
-
-// require('dotenv').config();
-
 // const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
 //     host: 'localhost',
 //     dialect: 'mysql',
 //     port: 3306
 // });
 
-// module.exports = sequelize;
+module.exports = sequelize;
